a99eceb6cc23ebddee5fd61c8fa7dffdce431ca6
Muhammad Mudassir <mudassir24904@gmail.com>
2026-02-18 02:17:51 +0500

    Add files via upload
    A simple React practice project using React Router for navigation and Tailwind CSS for styling. It's a basic Single Page Application (SPA) with centered layouts and dark mode

---
+++ react-router-nav-pro/src/pages/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+      <h1 className='text-8xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-8xl '>About</h1>
+    </div>
+  )
+}
+
+export default about
---
+++ react-router-nav-pro/src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div>
+ <h1 className='text-8xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-8xl '>home</h1>
+    </div>
+  )
+}
+
+export default home
