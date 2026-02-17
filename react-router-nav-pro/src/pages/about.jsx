55a43bdeae43f636461563f876ed688ad07cf56a
Muhammad Mudassir <mudassir24904@gmail.com>
2026-02-18 02:49:52 +0500

    Add files via upload

--- react-router-nav-pro/src/pages/about.jsx
+++ react-router-nav-pro/src/pages/about.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const about = () => {
   return (
     <div>
-      <h1 className='text-8xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-8xl '>About</h1>
+      <h1 className='text-8xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  '>About</h1>
     </div>
   )
 }
--- react-router-nav-pro/src/pages/home.jsx
+++ react-router-nav-pro/src/pages/home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const home = () => {
   return (
     <div>
- <h1 className='text-8xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-8xl '>home</h1>
+ <h1 className='text-8xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  '>home</h1>
     </div>
   )
 }
