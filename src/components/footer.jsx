673af54eb09c34efcf639379c4a242044368efed
Muhammad Mudassir <mudassir24904@gmail.com>
2026-02-18 22:09:56 +0500

    Delete src directory

--- src/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const footer = () => {
-  return (
-<footer className='bg-red-400  text-white fixed bottom-0 w-full text-5xl font-bold px-10 py-5'>footer</footer>
-  )
-}
-
-export default footer
